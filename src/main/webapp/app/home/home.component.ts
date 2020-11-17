4e5db28c54d692839b2e61f08dd26fd72d364663
achablowski <a.chablowski@gmail.com>
2020-11-17 17:55:52 +0100

    Manage unsubscribe with decorator

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,38 +1,31 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
-
-import { LoginModalService } from 'app/core/login/login-modal.service';
-import { AccountService } from 'app/core/auth/account.service';
-import { Account } from 'app/core/user/account.model';
+import { Component, OnInit } from '@angular/core';
 import { ShopService } from '../entities/shop/shop.service';
 import { HttpResponse } from '@angular/common/http';
 import { IShop } from '../shared/model/shop.model';
-import { ParentComponent } from 'app/shared/parent/parent.component';
-import { takeUntil } from 'rxjs/operators';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 
+@UntilDestroy()
 @Component({
   selector: 'jhi-home',
   templateUrl: './home.component.html',
   styleUrls: ['home.scss'],
 })
-export class HomeComponent extends ParentComponent implements OnInit {
+export class HomeComponent implements OnInit {
   shops?: IShop[];
 
-  constructor(protected shopService: ShopService) {
-    super();
-  }
+  constructor(protected shopService: ShopService) {}
 
   loadShops(): void {
     this.shopService
       .query()
-      .pipe(takeUntil(this.destroy$))
+      .pipe(untilDestroyed(this))
       .subscribe((res: HttpResponse<IShop[]>) => (this.shops = res.body || []));
   }
 
   ngOnInit(): void {
     this.shopService
       .query()
-      .pipe(takeUntil(this.destroy$))
+      .pipe(untilDestroyed(this))
       .subscribe((res: HttpResponse<IShop[]>) => (this.shops = res.body || []));
   }
 
